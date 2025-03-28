c69de6fb70118c8e870539e635e82e33c686d157
artaasadi <arta-c@kaytu.io>
2025-03-28 21:58:15 +0100

    fix: add platform_artifact_sbom table

--- task/resource.go
+++ task/resource.go
@@ -1,49 +1,12 @@
 package task
 
 type ArtifactSbom struct {
-	ImageURL   string      `json:"imageUrl"`
-	ArtifactID string      `json:"artifactId"`
-	SbomFormat string      `json:"sbomFormat"`
+	ImageURL   string      `json:"image_url"`
+	ArtifactID string      `json:"artifact_id"`
+	SbomFormat string      `json:"sbom_format"`
 	Sbom       interface{} `json:"sbom"`
 }
 
 func (r ArtifactSbom) UniqueID() string {
 	return r.ArtifactID
-}
-
-type GrypeOutput struct {
-	Matches []VulnerabilityMatch `json:"matches"`
-}
-
-type VulnerabilityMatch struct {
-	Vulnerability          Vulnerability   `json:"vulnerability"`
-	RelatedVulnerabilities []Vulnerability `json:"relatedVulnerabilities"`
-	MatchDetail            interface{}     `json:"matchDetail"`
-	Artifact               interface{}     `json:"artifact"`
-}
-
-type Vulnerability struct {
-	ID          string             `json:"id"`
-	DataSource  string             `json:"dataSource"`
-	Namespace   string             `json:"namespace"`
-	Severity    string             `json:"severity"`
-	URLs        []string           `json:"urls"`
-	Description string             `json:"description"`
-	CVSs        []VulnerabilityCVS `json:"cvss"`
-	Fix         VulnerabilityFix   `json:"fix"`
-	Advisories  interface{}        `json:"advisories"`
-}
-
-type VulnerabilityCVS struct {
-	Source         string            `json:"source"`
-	Type           string            `json:"type"`
-	Version        string            `json:"version"`
-	Vector         string            `json:"vector"`
-	Metrics        map[string]string `json:"metrics"`
-	VendorMetadata map[string]string `json:"vendorMetadata"`
-}
-
-type VulnerabilityFix struct {
-	Versions []string `json:"versions"`
-	State    string   `json:"state"`
 }
